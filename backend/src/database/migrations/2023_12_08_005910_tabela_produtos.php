97260f6c4c775c6b7300fb7d94610838845dbe5d
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-07 22:15:25 -0300

    comecando backend

---
+++ backend/src/database/migrations/2023_12_08_005910_tabela_produtos.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        schema::create('produtos', function(){
+            $table->id('id');
+            $table->string('nome_do_produto', 150);
+            $table->foreignId('categoria_id', );
+            $table->float('valor_do_produto');
+            $table->date('data_de_vencimento');
+            $table->integer('quantidade_em_estoque');
+            $table->boolean('produto_perecivel');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
