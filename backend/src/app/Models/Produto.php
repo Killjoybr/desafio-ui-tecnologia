2da6d5c1abf5ca3088fb8ceaad33af3261bb9113
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-08 00:03:29 -0300

    adicionando schema da tabela produto

---
+++ backend/src/app/Models/Produto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    protected $table = 'produtos';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nome_do_produto',
+        'categoria_id',
+        'valor_do_produto',
+        'data_de_vencimento',
+        'quantidade_em_estoque',
+        'produto_perecivel'
+    ];
+
+    public $timestamps = false;
+}
