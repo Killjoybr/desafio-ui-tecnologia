894b97f595a4b9488d8229e845fcf8b41d160691
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-07 22:16:42 -0300

    modelando banco de dados

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,20 @@
 -- No campo valor, utilizar pontos flutuantes pode trazer inconsistência nas operações/transações
 -- pesquisei sobre boas práticas e decidi trocar pelo tipo Money, presente no PostgreSQL.
+-- Devido ao tipo money não aparecer nas migrations do Laravel, voltei ao tipo float. 
 
-CREATE TABLE produtos(
-    id serial primary key,
-    nome_do_produto VARCHAR(150) not null,
-    categoria_id integer foreign key default 1,
-    valor_do_produto money not null,
-    data_de_vencimento date not null,
-    quantidade_em_estoque integer not null,
-    produto_perecivel boolean not null default false
-);
+CREATE DATABASE laravel;
 
 CREATE TABLE categorias(
     id serial primary key,
     descricao VARCHAR(150) not null
+);
+
+CREATE TABLE produtos(
+    id serial primary key,
+    nome_do_produto VARCHAR(150) not null,
+    categoria_id integer references categorias(id),
+    valor_do_produto float not null,
+    data_de_vencimento date not null,
+    quantidade_em_estoque integer not null,
+    produto_perecivel boolean not null default false
 );
