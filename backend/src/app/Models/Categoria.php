0715e7230b208ee3650ede108b37f6e1840e4ad3
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-08 10:55:21 -0300

    adicionando modelo de categoria

---
+++ backend/src/app/Models/Categoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categorias';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = 'descricao';
+
+    public $timestamps = false;
+
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class);
+    }
+}
