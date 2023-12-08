4edc55160b45426eb8442683cd0b3ef1dec8a22e
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-08 00:04:35 -0300

    adicionando as queries da tabela produtos

--- backend/src/app/Http/Controllers/controladorProdutos.php
+++ backend/src/app/Http/Controllers/controladorProdutos.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class controladorProdutos extends Controller
 {
+    public function index()
+    {
+        return Produto::all();
+    }
+
+    public function show($id)
+    {
+        return Produto::findOrFail($id);
+    }
     
+    public function store(Request $request)
+    {
+        return Produto::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->update($request->all());
+        
+        return $produto;
+    }
+
+    public function destroy($id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->delete();
+
+        return response()->json(null, 204);
+    }
 }
