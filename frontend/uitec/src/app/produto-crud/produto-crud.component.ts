c7f5e6fc6629001a7cedc2219a06380f85be417a
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-10 17:16:41 -0300

    corrigindo types e adicionando funcionalidades

--- frontend/uitec/src/app/produto-crud/produto-crud.component.ts
+++ frontend/uitec/src/app/produto-crud/produto-crud.component.ts
@@ -7,8 +7,22 @@ import { ApiService } from '../api.service';
   styleUrl: './produto-crud.component.scss'
 })
 export class ProdutoCrudComponent {
-  categorias: any[] = [];
-  produtos: any[] = [];
+  categorias: {id:number, descricao:string}[] = [];
+  produtos: {
+    id:number,
+    nome_do_produto:string,
+    categoria_id:number,
+    valor_do_produto:number,
+    data_de_vencimento:Date,
+    quantidade_em_estoque:number,
+    produto_perecivel:boolean
+  }[] = [];
+  nome_do_produto!:string;
+  categoria_id!:number;
+  valor_do_produto!:number;
+  data_de_vencimento!:Date;
+  quantidade_em_estoque!:number;
+  produto_perecivel:boolean = false;
 
   constructor(private apiService: ApiService) {}
 
@@ -21,5 +35,44 @@ export class ProdutoCrudComponent {
       this.produtos = data;
     });
   }
+
+  onSubmit() {
+    const formData = {
+      nome_do_produto: this.nome_do_produto,
+      categoria_id: this.categoria_id,
+      valor_do_produto: this.valor_do_produto,
+      data_de_vencimento: this.data_de_vencimento,
+      quantidade_em_estoque: this.quantidade_em_estoque,
+      produto_perecivel: this.produto_perecivel,
+    };
+    
+    this.apiService.cadastrarProduto(formData).subscribe(
+      {
+        error: (erro) => console.error('Erro ao criar produto:',erro),
+        complete: () => console.info('Produto cadastrado com sucesso', this.apiService.getProdutos().subscribe(data =>
+          {
+          this.produtos = data;
+          }
+        ))
+      }
+    );
+  }
+
+  deletarProduto(idProduto:number) {
+    this.apiService.deletarProduto(idProduto).subscribe(
+      {
+        error: (erro) => console.error('Erro ao deletar produto:', erro),
+        complete: () => console.info(`Produto de id:${idProduto} deletado com sucesso`,this.apiService.getProdutos().subscribe(data =>
+          {
+            this.produtos = data;
+          }
+        ))
+      }
+    )
+  }
+
+  // atualizarProduto(idProduto:number) {
+  //   this.apiService.atualizarProduto()
+  // }
 }
 
