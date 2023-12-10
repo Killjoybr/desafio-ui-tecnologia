66d00f5c8ada6c62a2c96798b4741b3b9bd2bafe
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-10 16:59:46 -0300

    adicionando categorias

--- db/dump.sql
+++ db/dump.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE laravel;
+CREATE DATABASE IF NOT EXISTS laravel;
 
-CREATE TABLE categorias(
+CREATE TABLE IF NOT EXISTS categorias(
     id serial primary key,
     descricao VARCHAR(150) not null
 );
 
-CREATE TABLE produtos(
+CREATE TABLE IF NOT EXISTS produtos(
     id serial primary key,
     nome_do_produto VARCHAR(150) not null,
     categoria_id integer references categorias(id),
@@ -14,3 +14,12 @@ CREATE TABLE produtos(
     quantidade_em_estoque integer not null,
     produto_perecivel boolean not null default false
 );
+
+INSERT INTO categorias(descricao) VALUES
+('Outros'),
+('Eletrônicos'),
+('Alimentos e Bebidas'),
+('Automotivo'),
+('Acessórios'),
+('Roupas'),
+('Brinquedos e Jogos')
