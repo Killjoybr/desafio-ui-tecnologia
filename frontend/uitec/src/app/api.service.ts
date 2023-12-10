4a4a7e754db7d0e1e5f7ee0869f900f882c49c9d
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2023-12-10 17:15:28 -0300

    adicionando cadastro, destruicao e atualizacao de produtos

--- frontend/uitec/src/app/api.service.ts
+++ frontend/uitec/src/app/api.service.ts
@@ -16,4 +16,18 @@ export class ApiService {
   getProdutos(): Observable<any> {
     return this.http.get(`${this.apiUrl}/produtos`)
   }
+
+  cadastrarProduto(dadosProduto:{nome_do_produto:string,categoria_id:number,valor_do_produto:number,data_de_vencimento:Date,quantidade_em_estoque:number,produto_perecivel:boolean}): Observable<any> {
+    return this.http.post(`${this.apiUrl}/produtos`, dadosProduto)
+  }
+
+
+  atualizarProduto(id:number, dadosProduto:{nome_do_produto:string,categoria_id:number,valor_do_produto:number,data_de_vencimento:Date,quantidade_em_estoque:number,produto_perecivel:boolean}): Observable<any> {
+    return this.http.put(`${this.apiUrl}/produtos/${id}`, dadosProduto)
+  }
+
+  deletarProduto(id:number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/produtos/${id}`)
+  }
+
 }
